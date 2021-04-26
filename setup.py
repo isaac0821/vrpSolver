8f6229f60d4114d36ee38bbfaad45dd695ebada0
isaac0821 <isaac0821@sina.com>
2021-04-26 12:00:28 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+  
+import setuptools
+from vrpSolver import __version__
+
+long_description = """
+	Working project: Learn VRP by coding
+"""
+
+setuptools.setup(
+	name="vrpSolver",
+	version=__version__,
+	author="Lan Peng",
+	author_email="lanpeng@buffalo.edu",
+	description="VRP Solver",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/isaac0821/vrpSolver",
+	license='MIT', 
+	packages=['vrpSolver'], 
+	download_url = "https://github.com/isaac0821/vrpSolver",
+	project_urls={
+		"Bug Tracker": "https://github.com/isaac0821/vrpSolver/issues",
+		"Documentation": "https://veroviz.org",
+		"Source Code": "https://github.com/isaac0821/vrpSolver",
+	},
+	python_requires='>=3',
+	install_requires=[
+		'numpy',  
+		'scipy',
+		'gurobipy',
+		'matplotlib'
+	],
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	]
+)
--- vrpSolver/__init__.py
+++ vrpSolver/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.0.1"
+
 from .common import *
 from .const import *
 from .plot import *
